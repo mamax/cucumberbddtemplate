590016606fe726bc10b8a8327d1d900310ead6e9
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2019-04-09 12:51:04 +0300

    Page Object Manager

---
+++ src/test/java/com/tools/qa/pageObjects/PageObjectManager.java
@@ -0,0 +1,36 @@
+package com.tools.qa.pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageObjectManager {
+    private WebDriver driver;
+
+    private ProductListingPage productListingPage;
+
+    private CartPage cartPage;
+
+    private HomePage homePage;
+
+    private CheckoutPage checkoutPage;
+
+    public PageObjectManager(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public HomePage getHomePage(){
+        return (homePage == null) ? homePage = new HomePage(driver) : homePage;
+    }
+
+    public ProductListingPage getProductListingPage() {
+        return (productListingPage == null) ? productListingPage = new ProductListingPage(driver) : productListingPage;
+    }
+
+
+    public CartPage getCartPage() {
+        return (cartPage == null) ? cartPage = new CartPage(driver) : cartPage;
+    }
+
+    public CheckoutPage getCheckoutPage() {
+        return (checkoutPage == null) ? checkoutPage = new CheckoutPage(driver) : checkoutPage;
+    }
+}
--- src/test/java/com/tools/qa/stepdefs/MyStepdefs.java
+++ src/test/java/com/tools/qa/stepdefs/MyStepdefs.java
@@ -3,6 +3,7 @@ package com.tools.qa.stepdefs;
 import com.tools.qa.pageObjects.CartPage;
 import com.tools.qa.pageObjects.CheckoutPage;
 import com.tools.qa.pageObjects.HomePage;
+import com.tools.qa.pageObjects.PageObjectManager;
 import com.tools.qa.pageObjects.ProductListingPage;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -14,10 +15,11 @@ import java.util.concurrent.TimeUnit;
 
 public class MyStepdefs {
     WebDriver driver;
-    private HomePage home;
-    private ProductListingPage productListingPage;
-    private CartPage cartPage;
-    private CheckoutPage checkoutPage;
+    HomePage homePage;
+    ProductListingPage productListingPage;
+    CartPage cartPage;
+    CheckoutPage checkoutPage;
+    PageObjectManager pageObjectManager;
 
     @Given("^user is on Home Page$")
     public void userIsOnTheHomePage() throws Throwable{
@@ -25,50 +27,48 @@ public class MyStepdefs {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://www.shop.demoqa.com");
+        pageObjectManager = new PageObjectManager(driver);
+        homePage = pageObjectManager.getHomePage();
+        homePage.navigateTo_HomePage();
     }
 
     @When("^he search for \"([^\"]*)\"$")
     public void heSearchForDress(String product) throws Throwable {
-        home = new HomePage(driver);
-        home.perform_Search(product);
+        homePage.perform_Search(product);
     }
 
     @And("^choose to buy the first item$")
     public void chooseToBuyTheFirstItem() throws Throwable {
-        productListingPage = new ProductListingPage(driver);
+        productListingPage = pageObjectManager.getProductListingPage();
         productListingPage.select_Product(0);
         productListingPage.clickOn_AddToCart();
     }
 
     @And("^moves to checkout from mini cart$")
     public void movesToCheckoutFromMiniCart() throws Throwable {
-        cartPage = new CartPage(driver);
+        cartPage = pageObjectManager.getCartPage();
         cartPage.clickOn_Cart();
         cartPage.clickOn_ContinueToCheckout();
     }
 
     @And("^enter personal details on checkout page$")
     public void enterPersonalDetailsOnCheckoutPage() throws Throwable {
-        checkoutPage = new CheckoutPage(driver);
+        checkoutPage = pageObjectManager.getCheckoutPage();
         checkoutPage.fill_PersonalDetails();
     }
 
     @And("^select same delivery address$")
     public void selectSameDeliveryAddress() throws Throwable {
-        checkoutPage = new CheckoutPage(driver);
         checkoutPage.check_ShipToDifferentAddress(false);
     }
 
     @And("^select payment method as \"([^\"]*)\" payment$")
     public void selectPaymentMethodAsCheckPayment(String buttonName) throws Throwable {
-        checkoutPage = new CheckoutPage(driver);
         checkoutPage.select_PaymentMethod(buttonName);
     }
 
     @And("^place the order$")
     public void placeTheOrder() throws Throwable {
-        checkoutPage = new CheckoutPage(driver);
         checkoutPage.check_TermsAndCondition(true);
         checkoutPage.clickOn_PlaceOrder();
 
